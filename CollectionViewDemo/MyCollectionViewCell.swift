96e45acac707fb960e7d264661aaac7ed70ab584
Narayan <SFN@Mosambees-MacBook-Pro.local>
2019-12-04 11:00:59 +0530

    first commit

---
+++ CollectionViewDemo/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Synergistic Financial Networks on 02/12/2019.
+//  Copyright © 2019 Synergistic Financial Networks. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViewDemo/MyImageView.swift
@@ -0,0 +1,21 @@
+//
+//  MyImageView.swift
+//  CollectionViewDemo
+//
+//  Created by Synergistic Financial Networks on 02/12/2019.
+//  Copyright © 2019 Synergistic Financial Networks. All rights reserved.
+//
+
+import UIKit
+
+class MyImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
