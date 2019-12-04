6b3349304cbbcb1482a41ad25fe5d0c35a5a8ab0
Narayan <narayan8692b@gmail.com>
2019-12-04 12:22:45 +0530

    Hi

--- CollectionViewDemo/MyImageView.swift
+++ CollectionViewDemo/MyImageView.swift
@@ -8,14 +8,59 @@
 
 import UIKit
 
+
 class MyImageView: UIImageView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    var imageURL: URL?
 
+    let activityIndicator = UIActivityIndicatorView()
+
+    func loadImageWithUrl(_ url: URL) {
+
+        // setup activityIndicator...
+        activityIndicator.color = .darkGray
+
+        addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+
+        imageURL = url
+
+        image = nil
+        activityIndicator.startAnimating()
+
+        // retrieves image if already available in cache
+        if let imageFromCache = imageCache.object(forKey: url as AnyObject) as? UIImage {
+
+            self.image = imageFromCache
+            activityIndicator.stopAnimating()
+            return
+        }
+
+        // image does not available in cache.. so retrieving it from url...
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+
+            if error != nil {
+                print(error as Any)
+                DispatchQueue.main.async(execute: {
+                    self.activityIndicator.stopAnimating()
+                })
+                return
+            }
+
+            DispatchQueue.main.async(execute: {
+
+                if let unwrappedData = data, let imageToCache = UIImage(data: unwrappedData) {
+
+                    if self.imageURL == url {
+                        self.image = imageToCache
+                    }
+
+                    imageCache.setObject(imageToCache, forKey: url as AnyObject)
+                }
+                self.activityIndicator.stopAnimating()
+            })
+        }).resume()
+    }
 }
